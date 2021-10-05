e99c997cf9175b369e63051121629bb114846904
Anita Rosikhina <anita2.sergeevna@gmail.com>
2021-10-05 14:27:28 +0300

    first commit

---
+++ src/app/cenzor/cenzor.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cenzor',
+  templateUrl: './cenzor.component.html',
+  styleUrls: ['./cenzor.component.css']
+})
+export class CenzorComponent {
+
+  input: any
+  textarea: any
+  showError: boolean = false
+  showErrorTextarea: boolean = false
+  badWords: string[] = []
+
+  addBadWord() {
+    if (!this.input) {
+      this.showError = true
+    } else {
+      this.badWords.push(this.input)
+      this.showError = false
+      this.input = ''
+    }
+  }
+
+  resetBadWords() {
+    this.badWords = []
+  }
+
+  censor() {
+    if (!this.textarea) {
+      this.showErrorTextarea = true
+    }
+    else {
+      this.showErrorTextarea = false
+      this.textarea = this.textarea
+        .split(' ')
+        .map((el: string) => this.badWords.some(elem => elem === el) ? '*'.repeat(el.length) : el)
+        .join(' ');
+    }
+  }
+
+}
---
+++ src/app/userlist/userlist.component.ts
@@ -0,0 +1,96 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-userlist',
+  templateUrl: './userlist.component.html',
+  styleUrls: ['./userlist.component.css']
+})
+export class UserlistComponent implements OnInit {
+
+  login = ''
+  password = ''
+  email = ''
+  error = false
+
+  users: IUser[] = []
+  addState = true
+  editingIndex: number = 0
+
+  @ViewChild('emailInput', {static: true}) emailRef?: ElementRef
+
+  ngOnInit() {
+    if (this.emailRef) {
+      console.log(this.emailRef);
+      this.emailRef.nativeElement.style.borderColor = 'gray'
+    }
+  }
+
+  get loginIsValid(): boolean {
+    return !!this.login && new RegExp(/^\S+[a-zA-Z]{3,16}$/).test(this.login)
+  }
+
+  get passwordIsValid(): boolean {
+    return !!this.password && new RegExp(/^\S{4,16}/).test(this.password)
+  }
+
+  get emailIsValid(): boolean {
+    return !!this.email && new RegExp(/\S+@\S+\.\S+/).test(this.email)
+  }
+
+  get checkFormValidity(): boolean {
+    return this.loginIsValid && this.passwordIsValid && this.emailIsValid
+  }
+
+  addUser() {
+    if (this.checkFormValidity) {
+      let info: IUser = {
+        login: this.login,
+        password: this.password,
+        email: this.email,
+      }
+
+      this.error = false
+      this.users.push(info)
+      this.reset()
+    } else if (!this.checkFormValidity) {
+      this.error = true
+    }
+  }
+
+  reset(): void {
+    this.login = ''
+    this.password = ''
+    this.email = ''
+  }
+
+  setFieldsForEditUser(user: IUser, index: number) {
+    this.addState = false
+    this.login = user.login
+    this.password = user.password
+    this.email = user.email
+
+    console.log(this.emailRef?.nativeElement.style.borderColor);
+    // login.style.borderColor =
+    //   password.style.borderColor =
+    //     email.style.borderColor = 'gray'
+
+    this.editingIndex = index
+  }
+
+  deleteUser(index: number) {
+    this.users.splice(index, 1)
+  }
+
+  editUser() {
+    if (this.checkFormValidity) {
+      this.addState = true
+
+      this.users[this.editingIndex] = {
+        login: this.login,
+        password: this.password,
+        email: this.email,
+      }
+      this.reset()
+    }
+  }
+}
